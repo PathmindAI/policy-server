da259a33facf0a68da981abad6e770738c553ea5
Edward Junprung <edward@skymind.io>
2020-07-07 15:34:53 -0700

    Update zinc factory example with working policy

--- examples/zinc_factory/preprocessor.py
+++ examples/zinc_factory/preprocessor.py
@@ -1,9 +1,8 @@
 def preprocess(obs):
     return [
-        obs.get("hour") / 24,
-        obs.get("day") / 365,
-        obs.get("month") / 12,
-        obs.get("30_minute_cost") / 1000,
-        obs.get("price") / 40,
-        obs.get("total_cost") / 1000000,
+        obs.get("minute"),
+        obs.get("hour"),
+        obs.get("day"),
+        obs.get("month"),
+        obs.get("price"),
     ]
