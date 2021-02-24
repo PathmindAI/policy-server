9f4746f37e4b02497e0201727c9cea7aa3d26a83
Max Pumperla <max.pumperla@googlemail.com>
2021-02-24 20:43:49 +0100

    add raw endpoint

--- app.py
+++ app.py
@@ -7,26 +7,27 @@ import yaml
 import numpy as np
 
 import config
-from api import Action, Observation, Experience
+from api import Action, Observation, Experience, RawObservation
 from api import _predict_deterministic, _distribution, _predict
 from typing import List
 from utils import unzip
 from generate import CLI
+from offline import EpisodeCache
+from security import get_api_key
 
-import secrets
 from fastapi.responses import FileResponse
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, FastAPI, HTTPException, status, Security
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
 from fastapi.openapi.utils import get_openapi
 
 from ray.rllib.evaluation.sample_batch_builder import SampleBatchBuilder
 from ray.rllib.offline.json_writer import JsonWriter
-from offline import EpisodeCache
 
 cache = EpisodeCache()
 batch_builder = SampleBatchBuilder()  # or MultiAgentSampleBatchBuilder
 writer = JsonWriter(config.EXPERIENCE_LOCATION)
 
+app = FastAPI()
 
 tags_metadata = [
     {
@@ -45,23 +46,6 @@ tags_metadata = [
 
 
 SERVE_HANDLE = None
-
-app = FastAPI()
-
-security = HTTPBasic()
-
-
-def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, config.USER_NAME)
-    correct_password = secrets.compare_digest(credentials.password, config.PASSWORD)
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return True
-
 
 unzip(config.PATHMIND_POLICY)
 
@@ -99,94 +83,97 @@ if config.USE_RAY:
         SERVE_HANDLE = client.get_handle("predict")
 
 
-@app.post("/predict/", response_model=Action, tags=["Predictions"])
-async def predict(payload: Observation, logged_in: bool = Depends(verify_credentials)):
-    if config.USE_RAY:
-
+if config.observations:
+    # Note: for basic auth, use "logged_in: bool = Depends(verify_credentials)" as parameter
+    @app.post("/predict/", response_model=Action, tags=["Predictions"])
+    async def predict(payload: Observation, api_key: APIKey = Depends(get_api_key)):
         lists = [[getattr(payload, obs)] if not isinstance(getattr(payload, obs), List) else getattr(payload, obs)
                  for obs in config.observations.keys()]
         observations = list(itertools.chain(*lists))
         # Note that ray can't pickle the pydantic "Observation" model, so we need to convert it here.
         return await SERVE_HANDLE.remote(observations)
-    else:
-        return _predict(payload)
 
 
-@app.post("/collect_experience/", response_model=Action, tags=["Predictions"])
-async def collect_experience(payload: Experience, logged_in: bool = Depends(verify_credentials)):
+    @app.post("/predict_deterministic/", response_model=Action, tags=["Predictions"])
+    async def predict_deterministic(payload: Observation, api_key: APIKey = Depends(get_api_key)):
+        return _predict_deterministic(payload)
 
-    global cache
-    global batch_builder
 
-    observation = payload.observation
-    rew = payload.reward
-    done = payload.done
+    @app.post("/distribution/", tags=["Predictions"])
+    async def distribution(payload: Observation, api_key: APIKey = Depends(get_api_key)):
+        return _distribution(payload)
 
-    lists = [[getattr(observation, obs)] if not isinstance(getattr(observation, obs), List) else getattr(observation, obs)
-             for obs in config.observations.keys()]
 
-    obs = list(itertools.chain(*lists))
-    obs = np.reshape(np.asarray(obs), (4,))
+    @app.post("/collect_experience/", response_model=Action, tags=["Predictions"])
+    async def collect_experience(payload: Experience, api_key: APIKey = Depends(get_api_key)):
 
-    action = _predict(obs)
+        global cache
+        global batch_builder
 
-    # from client import prep
-    # print("The preprocessor is", prep)
+        observation = payload.observation
+        rew = payload.reward
+        done = payload.done
 
-    if cache.is_empty():
-        cache.store(t=0, prev_obs=obs, prev_action=action.actions, prev_reward=rew)
+        lists = [[getattr(observation, obs)] if not isinstance(getattr(observation, obs), List) else getattr(observation, obs)
+                 for obs in config.observations.keys()]
 
-    act =action.actions[0]
+        obs = list(itertools.chain(*lists))
+        obs = np.reshape(np.asarray(obs), (4,))
 
-    batch_builder.add_values(
-        agent_index=0,
-        actions=act,
-        action_prob=action.probability,
-        action_logp=np.log(action.probability),
-        t=cache.t,
-        eps_id=cache.episode,
+        action = _predict(obs)
 
-        prev_actions=cache.prev_action,
-        prev_rewards=cache.prev_reward,
-        obs=cache.prev_obs,  # prep.transform(...)
+        # from client import prep
+        # print("The preprocessor is", prep)
 
-        # sent from environment
-        new_obs=obs,
-        dones=done,
-        infos=None,
-        rewards=rew,
-    )
-    cache.store(t=cache.t+1, prev_obs=obs, prev_action=act, prev_reward=rew)
+        if cache.is_empty():
+            cache.store(t=0, prev_obs=obs, prev_action=action.actions, prev_reward=rew)
 
-    if done:
-        writer.write(batch_builder.build_and_reset())
-        cache.reset()
+        act =action.actions[0]
 
-    return action
+        batch_builder.add_values(
+            agent_index=0,
+            actions=act,
+            action_prob=action.probability,
+            action_logp=np.log(action.probability),
+            t=cache.t,
+            eps_id=cache.episode,
+
+            prev_actions=cache.prev_action,
+            prev_rewards=cache.prev_reward,
+            obs=cache.prev_obs,  # prep.transform(...)
+
+            # sent from environment
+            new_obs=obs,
+            dones=done,
+            infos=None,
+            rewards=rew,
+        )
+        cache.store(t=cache.t+1, prev_obs=obs, prev_action=act, prev_reward=rew)
+
+        if done:
+            writer.write(batch_builder.build_and_reset())
+            cache.reset()
+
+        return action
+
+
+@app.post("/predict_raw/", response_model=Action, tags=["Predictions"])
+async def predict_raw(payload: RawObservation, api_key: APIKey = Depends(get_api_key)):
+    return await SERVE_HANDLE.remote(payload.obs)
 
 
 @app.get("/clients", tags=["Clients"])
-async def clients(logged_in: bool = Depends(verify_credentials)):
+async def clients(api_key: APIKey = Depends(get_api_key)):
     shutil.make_archive('clients', 'zip', './clients')
     return FileResponse(path='clients.zip', filename='clients.zip')
 
 
 @app.get("/schema", tags=["Clients"])
-async def server_schema(logged_in: bool = Depends(verify_credentials)):
+async def server_schema(api_key: APIKey = Depends(get_api_key)):
     with open(config.PATHMIND_SCHEMA, 'r') as schema_file:
         schema_str = schema_file.read()
     schema = yaml.safe_load(schema_str)
     return schema
-
-
-@app.post("/predict_deterministic/", response_model=Action, tags=["Predictions"])
-async def predict_deterministic(payload: Observation, logged_in: bool = Depends(verify_credentials)):
-    return _predict_deterministic(payload)
-
-
-@app.post("/distribution/", tags=["Predictions"])
-async def distribution(payload: Observation, logged_in: bool = Depends(verify_credentials)):
-    return _distribution(payload)
 
 
 @app.get("/", tags=["Health"])
