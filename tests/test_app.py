71fc04860d7831df2ba3afc5ddb48a5275dbca53
Slin Lee <slin@pathmind.com>
2021-06-29 14:19:34 -0700

    Get app tests to work

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,6 +14,16 @@ def test_health_check():
     assert response.json() == "ok"
 
 
+def test_docs():
+    response = client.get("/docs")
+    assert response.status_code == 200
+
+
+def test_redoc():
+    response = client.get("/redoc")
+    assert response.status_code == 200
+
+
 payload = {
     "mouse_row": 1,
     "mouse_col": 1,
@@ -24,36 +34,28 @@ payload = {
 }
 
 
-def test_predict():
+def test_predict_no_auth():
     response = client.post("http://localhost:8000/predict/", json=payload)
     assert response.status_code == 403
+
+
+def test_predict_bad_observations():
     response = client.post(
         "http://localhost:8000/predict/",
         json={"bad_payload": 1},
         headers={"access-token": "1234567asdfgh"},
     )
-    print(f"status code: {response.status_code}")
     assert response.status_code == 422
-    response = client.post(
-        "http://localhost:8000/predict/",
-        json=payload,
-        headers={"access-token": "1234567asdfgh"},
-    )
-    print(f"status code: {response.status_code}")
-    assert response.status_code == 200
-    # assert len(response.json()["actions"]) == 1
-    # assert response.json()["probability"] >= 0
-    assert response.json()
 
 
-# def test_predict(server):
-# get predictions
-# assert client code generated
-# assert docs url running
-#    predict()
-
-
-# res = requests.get("http://localhost:8000/openapi.json")
-
-# with open("../openapi.json", "w") as f:
-#    f.write(res.content.decode("utf-8"))
+def test_predict():
+    with TestClient(app) as client:
+        response = client.post(
+            "http://localhost:8000/predict/",
+            json=payload,
+            headers={"access-token": "1234567asdfgh"},
+        )
+        assert response.status_code == 200
+        assert response.json()
+        assert len(response.json()["actions"]) == 1
+        assert response.json()["probability"] >= 0
