0e0c2827a8eef98544433a0ec5640cf844d00b14
Slin Lee <slin@pathmind.com>
2021-06-29 10:28:18 -0700

    Revert "Remove seq_len from api call"
    This reverts commit f4761a3f2116244b03afb7e3683a8bb31aa05162.

--- api.py
+++ api.py
@@ -38,6 +38,7 @@ class PathmindPolicy:
             tf.zeros((), dtype=tf.int64), (), name="timestep"
         )
         self.prev_reward_tensor = tf.constant([0], dtype=tf.float32)
+        self.seq_lens_tensor = tf.constant([0], dtype=tf.int32)
 
         self.load_policy = tf.saved_model.load(config.TF_MODEL_PATH)
         self.model = self.load_policy.signatures.get("serving_default")
@@ -45,14 +46,11 @@ class PathmindPolicy:
     async def __call__(self, request):
         array = np.asarray(await request.body())
         op = np.reshape(array, (1, array.size))
-        tensors = tf.convert_to_tensor(op, dtype=tf.float32, name="observations")
+        tensors = tf.convert_to_tensor(op, dtype=tf.float32, name='observations')
 
         result = self.model(
-            is_training=self.is_training_tensor,
-            observations=tensors,
-            prev_action=self.prev_action_tensor,
-            prev_reward=self.prev_reward_tensor,
-            timestep=self.timestep,
+            is_training=self.is_training_tensor, observations=tensors, prev_action=self.prev_action_tensor,
+            prev_reward=self.prev_reward_tensor, seq_lens=self.seq_lens_tensor, timestep=self.timestep
         )
 
         action_keys = [k for k in result.keys() if "actions" in k]
