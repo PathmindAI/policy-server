cbde322cc734dbdee441ca727cc9979dd497699c
Slin Lee <slin@pathmind.com>
2021-06-29 10:27:24 -0700

    WIP update tests

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,4 +1,37 @@
+from multiprocessing import Process
+
+import pytest
 import requests
+import uvicorn
+from fastapi import FastAPI
+
+from ../app import *
+from ../generate import CLI
+
+res = requests.get("http://localhost:8000/openapi.json")
+
+with open("../openapi.json", "w") as f:
+    f.write(res.content.decode("utf-8"))
+###
+
+
+
+def run_server():
+    uvicorn.run(app)
+
+
+@pytest.fixture
+def server():
+    proc = Process(target=run_server, args=(), daemon=True)
+    proc.start()
+    yield
+    proc.kill() # Cleanup after test
+
+
+def test_read_main(server):
+    response = requests.get("http://localhost:8000/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
 
 payload = {
     "mouse_row": 1,
@@ -13,11 +46,6 @@ def predict():
     print(res.json())
     return res
 
-
-predict()
-
-
-res = requests.get("http://localhost:8000/openapi.json")
-
-with open("../openapi.json", "w") as f:
-    f.write(res.content.decode("utf-8"))
+def test_predict():
+    CLI.copy_server_files("examples/mouse_and_cheese")
+    predict()
